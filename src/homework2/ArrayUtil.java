d9eca7a6df2c464d3fb9b91e3a5c30f6256a6cf8
gohar harutyunian@gmail.com <goharharutyunian.gh@gmail.com>
2023-01-08 06:12:21 -0800

    ArrayUtil_homework

--- src/homework2/ArrayUtil.java
+++ src/homework2/ArrayUtil.java
@@ -5,21 +5,78 @@ public class ArrayUtil {
     public static void main(String[] args) {
         int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
 
+        System.out.println("First Element: " + array[0]);
+        int lastIndex = array.length - 1;
+        System.out.println("Last Element: " + array[lastIndex]);
+        System.out.print("All Elements: ");
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("Max: " + max);
 
-        int smallest = array[0];
-        int greatest = array[7];
-        if (array[0] < greatest) {
-            System.out.println("The smallest number is: " + smallest);
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
         }
-        if (array[7] > smallest) {
-            System.out.println("The greatest number is: " + greatest);
+        System.out.println("Min: " + min);
+
+        System.out.println();
+        System.out.println("Evens:");
+        for (int x : array) {
+            if (x % 2 == 0) {
+                System.out.print(x + " ");
+            }
         }
+        System.out.println();
+        System.out.println("Odds:");
+        for (int x : array) {
+            if (x % 2 != 0) {
+                System.out.println(x + " ");
+            }
+        }
+
+        int evenCount = 0;
+        for (int x : array) {
+            if (x % 2 == 0) {
+                evenCount++;
+            }
+        }
+        System.out.println("Number of pairs:" + evenCount);
+
+        int oddCount = 0;
+        for (int x : array) {
+            if (x % 2 == 0) {
+                oddCount++;
+            }
+        }
+        System.out.println("Number of odds:" + oddCount);
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
