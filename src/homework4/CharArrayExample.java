23394c769c57dea4ab6227a01eb408cf02847327
gohar harutyunian@gmail.com <goharharutyunian.gh@gmail.com>
2023-01-08 06:13:24 -0800

    homework4

---
+++ src/homework4/CharArrayExample.java
@@ -0,0 +1,26 @@
+package homework4;
+
+public class CharArrayExample {
+
+    public static void main(String[] args) {
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+
+        System.out.println(chars[(chars.length/2)-1] + " " + chars[chars.length/2]);
+
+        if (chars[chars.length-2] == 'l'
+        && chars[chars.length -1] == 'y') {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        char c = 'j';
+        int count = 0;
+        for (char a : chars) {
+            if (a == c) {
+                count++;
+            }
+        }
+        System.out.println("count: " + count);
+    }
+
+}
